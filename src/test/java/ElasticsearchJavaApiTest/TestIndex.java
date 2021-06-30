2f1ae65ced5e150bf27b0ae3ed14f083dc4c884e
yzhao <yzhao@fortinet.com>
2021-06-30 16:46:14 -0700

    TestIndex

--- src/test/java/ElasticsearchJavaApiTest/TestIndex.java
+++ src/test/java/ElasticsearchJavaApiTest/TestIndex.java
@@ -33,6 +33,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -119,10 +120,11 @@ public class TestIndex {
     @Test
     @Order(1)
     public void existsIndexBeforeDelete() throws IOException{
+        System.out.println("existsIndexBeforeDelete started");
         IndicesClient indices = restClient.indices();
         GetIndexRequest request = new GetIndexRequest("twitter");
         boolean exists = indices.exists(request, REQUEST_OPTIONS_DEFAULT);
-        assert(exists == true);
+        Assertions.assertTrue(exists);
     }
 
     /**
@@ -131,9 +133,10 @@ public class TestIndex {
     @Test
     @Order(2)
     public void deleteIndex() throws IOException{
+        System.out.println("deleteIndex started");
         DeleteIndexRequest request = new DeleteIndexRequest("twitter");
         AcknowledgedResponse deleteResp = restClient.indices().delete(request, REQUEST_OPTIONS_DEFAULT);
-        assert(deleteResp.isAcknowledged() == true);
+        Assertions.assertTrue(deleteResp.isAcknowledged());
     }
 
     /**
@@ -142,10 +145,11 @@ public class TestIndex {
     @Test
     @Order(3)
     public void existsIndexAfterDelete() throws IOException{
+        System.out.println("existsIndexAfterDelete started");
         IndicesClient indices = restClient.indices();
         GetIndexRequest request = new GetIndexRequest("twitter");
         boolean exists = indices.exists(request, REQUEST_OPTIONS_DEFAULT);
-        assert(exists == false);
+        Assertions.assertFalse(exists);
     }
 
     /**
@@ -154,13 +158,14 @@ public class TestIndex {
     @Test
     @Order(4)
     public void existsIndexAsync(){
+        System.out.println("existsIndexAsync started");
         IndicesClient indices = restClient.indices();
         GetIndexRequest request = new GetIndexRequest("twitter");
         indices.existsAsync(request, REQUEST_OPTIONS_DEFAULT,
                 new ActionListener<Boolean>() {
                     @Override
                     public void onResponse(Boolean result) {
-                        assert(result == false);;
+                        Assertions.assertFalse(result);
                     }
                     @Override
                     public void onFailure(Exception e) {
@@ -181,6 +186,7 @@ public class TestIndex {
     @Test
     @Order(5)
     public void createIndex() throws IOException{
+        System.out.println("createIndex started");
         CreateIndexRequest request = new CreateIndexRequest("twitter");
         // 设置settings
         request.settings(Settings.builder()
@@ -226,6 +232,8 @@ public class TestIndex {
         request.alias(new Alias("tw_alias"));
 
         CreateIndexResponse createResp = restClient.indices().create(request, REQUEST_OPTIONS_DEFAULT);
+        Assertions.assertTrue(createResp.isAcknowledged());
+        Assertions.assertEquals("twitter", createResp.index());
         System.out.println(createResp.isAcknowledged());
         System.out.println(createResp.index());
     }
@@ -237,6 +245,7 @@ public class TestIndex {
     @Test
     @Order(6)
     public void getIndexMapping() throws IOException{
+        System.out.println("getIndexMapping started");
         GetMappingsRequest request = new GetMappingsRequest();
         request.indices("twitter");
         GetMappingsResponse mappingResp = restClient.indices().getMapping(request, REQUEST_OPTIONS_DEFAULT);
@@ -244,6 +253,8 @@ public class TestIndex {
         Map<String, MappingMetaData> allMappings = mappingResp.mappings();
         MappingMetaData mappingMetaData = allMappings.get("twitter");
         Map<String, Object> map = mappingMetaData.sourceAsMap();
+        Assertions.assertEquals(1, allMappings.size());
+        Assertions.assertEquals("{properties={message={type=keyword}}}", map.toString());
         System.out.println(allMappings.size());
         System.out.println(map);
     }
@@ -254,8 +265,13 @@ public class TestIndex {
     @Test
     @Order(7)
     public void refreshIndex() throws IOException{
+        System.out.println("refreshIndex started");
         RefreshRequest request = new RefreshRequest("twitter");
         RefreshResponse refreshResp = restClient.indices().refresh(request, REQUEST_OPTIONS_DEFAULT);
+        Assertions.assertEquals(9, refreshResp.getTotalShards());
+        Assertions.assertEquals(3, refreshResp.getSuccessfulShards());
+        Assertions.assertEquals(0, refreshResp.getFailedShards());
+
         System.out.println(refreshResp.getTotalShards());
         System.out.println(refreshResp.getSuccessfulShards());
         System.out.println(refreshResp.getFailedShards());
@@ -266,12 +282,16 @@ public class TestIndex {
     @Test
     @Order(8)
     public void closeAndOpenIndex() throws IOException{
+        System.out.println("closeAndOpenIndex started");
         CloseIndexRequest closeReq = new CloseIndexRequest("twitter");
         AcknowledgedResponse close = restClient.indices().close(closeReq, REQUEST_OPTIONS_DEFAULT);
         System.out.println("close: " + close.isAcknowledged());
+        Assertions.assertTrue(close.isAcknowledged());
 
         OpenIndexRequest openReq = new OpenIndexRequest("twitter");
         OpenIndexResponse open = restClient.indices().open(openReq, REQUEST_OPTIONS_DEFAULT);
+        Assertions.assertTrue(open.isAcknowledged());
+        Assertions.assertTrue(open.isShardsAcknowledged());
         System.out.println("open: " + open.isAcknowledged());
         System.out.println(open.isShardsAcknowledged());
     }
@@ -284,6 +304,7 @@ public class TestIndex {
     @Test
     @Order(8)
     public void aliasIndex() throws IOException{
+        System.out.println("aliasIndex started");
         // 根据别名获取索引
         GetAliasesRequest getAliasesRequest = new GetAliasesRequest("tw_alias");
         GetAliasesResponse getAliasResp = restClient.indices().getAlias(getAliasesRequest, REQUEST_OPTIONS_DEFAULT);
@@ -295,14 +316,14 @@ public class TestIndex {
         AliasActions addAction = new AliasActions(AliasActions.Type.ADD).index("twitter").alias("alias2");
         aliasesRequest.addAliasAction(addAction);
         AcknowledgedResponse addAliases = restClient.indices().updateAliases(aliasesRequest, REQUEST_OPTIONS_DEFAULT);
-        assert(addAliases.isAcknowledged() == true);
+        Assertions.assertTrue(addAliases.isAcknowledged());
         System.out.println("add aliase: " + addAliases.isAcknowledged());
 
         // 删除别名
         AliasActions delAction = new AliasActions(AliasActions.Type.REMOVE).index("twitter").alias("alias2");
         aliasesRequest.addAliasAction(delAction);
         AcknowledgedResponse delAliases = restClient.indices().updateAliases(aliasesRequest, REQUEST_OPTIONS_DEFAULT);
-        assert(addAliases.isAcknowledged() == true);
+        Assertions.assertTrue(addAliases.isAcknowledged());
         System.out.println("delete alias: " + delAliases.isAcknowledged());
     }
 
